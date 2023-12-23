7a3ee06a62732baa1d086ad2997ab78e9414d341
PScoriae <pierreccesario@gmail.com>
2023-12-23 14:02:10 +0800

    fix: use full brew commands

--- install.sh
+++ install.sh
@@ -5,9 +5,6 @@ if ! command -v brew &> /dev/null; then
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
-alias bi="brew install"
-alias bic="brew install --cask"
-
 # List of common developer tools
 tools=("git" "gh" "go" "jq" "yq" "gnu-sed" "k9s" "kubectx" "kubernetes-cli" "kustomize" "nerdctl" "neovim" "node" "terraform" "helm" "hugo")
 
@@ -22,22 +19,22 @@ casks=("discord" "github" "iina" "iterm2" "linearmouse" "obsidian" "postman" "ta
 
 # Install common developer tools
 for tool in "${tools[@]}"; do
-  bi "$tool"
+  brew install "$tool"
 done
 
 # Install zsh stuff
 for zsh in "${zshStuff[@]}"; do
-  bi "$zsh"
+  brew install "$zsh"
 done
 
 # Install favorite tools
 for tool in "${favTools[@]}"; do
-  bi "$tool"
+  brew install "$tool"
 done
 
 # Install casks
 for cask in "${casks[@]}"; do
-  bic "$cask"
+  brew install --cask "$cask"
 done
 
 echo "Installation complete."
